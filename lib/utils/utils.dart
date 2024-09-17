8780b9a90a9459d5edcb7bc2257678f8e79ca8c3
Adeel Ahmad Khan <97823437+AdeelAhmadKhan36@users.noreply.github.com>
2024-09-17 22:05:37 +0500

    Utils FiledChanged

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,27 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
-void showCustomSnackbar(String message) {
-  Get.rawSnackbar(
-    backgroundColor: Colors.red,
+class  Utils{
+    //Control Automatically Shift from Email to Password
+   static void FieldonChnage(BuildContext context, FocusNode current, FocusNode nextFocus){
 
-    duration: const Duration(seconds: 3),
-    margin: const EdgeInsets.only(bottom: 770, left: 16.0, right: 16.0), // Top margin and left-right padding
-    padding: const EdgeInsets.symmetric(horizontal: 16.0), // Padding inside the snackbar
-    messageText: Container(
-      height: 60,
-      child: Center(
-        child: Text(
-          message,
-          style: const TextStyle(
-            fontSize: 16.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.white, // Set text color here
+     current.unfocus();
+     FocusScope.of(context).requestFocus(nextFocus);
+   }
+
+
+
+
+
+  void showCustomSnackbar(String message) {
+    Get.rawSnackbar(
+      backgroundColor: Colors.red,
+
+      duration: const Duration(seconds: 3),
+      margin: const EdgeInsets.only(bottom: 770, left: 16.0, right: 16.0), // Top margin and left-right padding
+      padding: const EdgeInsets.symmetric(horizontal: 16.0), // Padding inside the snackbar
+      messageText: Container(
+        height: 60,
+        child: Center(
+          child: Text(
+            message,
+            style: const TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white, // Set text color here
+            ),
           ),
         ),
       ),
-    ),
-    borderRadius: 8.0, // Optional: Adds rounded corners
-  );
+      borderRadius: 8.0, // Optional: Adds rounded corners
+    );
+  }
 }
+
 
